0889a28c184345a6042fab3971f50a122c9f33db
Joe <joelowrance@gmail.com>
2021-05-31 12:26:11 -0400

    Add service bus topic function

--- ServiceBusQueueFunction/MyTrigger.cs
+++ ServiceBusQueueFunction/MyTrigger.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -9,8 +10,15 @@ namespace ServiceBusQueueFunction
     public static class MyTrigger
     {
         [FunctionName("MyTrigger")]
+        // myQueueItem can be string
+        //public static async Task RunAsync([ServiceBusTrigger("mainqueue", Connection = "sb-connection")]
+        //    string myQueueItem, ILogger log)
+
+        //  myQueue Itemcan be Message
         public static async Task RunAsync([ServiceBusTrigger("mainqueue", Connection = "sb-connection")]
-            string myQueueItem, ILogger log)
+            Message myQueueItem, ILogger log)
+
+        
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
             
---
+++ ServiceBusQueueFunction/TopicTrigger.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace ServiceBusQueueFunction
+{
+    public static class TopicTrigger
+    {
+        [FunctionName("TopicTrigger")]
+        public static async Task RunAsync([ServiceBusTrigger("mytopic", "Sub1", Connection = "topic-connection")]
+            string mySbMsg, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+            
+        }
+    }
+}
