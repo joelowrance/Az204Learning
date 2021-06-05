8f49a7c953360d08fa0071a39fa8cf5328215315
Joe <joelowrance@gmail.com>
2021-06-05 09:16:32 -0400

    Add basic generic web application

---
+++ GenericWebApplication/Models/Course.cs
@@ -0,0 +1,9 @@
+namespace GenericWebApplication.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
---
+++ GenericWebApplication/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace GenericWebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args)
+                .ConfigureAppConfiguration((cx, builder) =>
+                {
+                    if (cx.HostingEnvironment.IsDevelopment())
+                    {
+                        builder.AddUserSecrets<Program>();
+                    }
+                }).Build();
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
