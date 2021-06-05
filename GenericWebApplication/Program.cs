7baf9d9b1bee0b2bb6f236014a6c69a45f10318d
Joe <joelowrance@gmail.com>
2021-06-05 19:38:26 -0400

    App insights configuration trial and error

--- GenericWebApplication/Program.cs
+++ GenericWebApplication/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Azure.Identity;
+using System;
 
 namespace GenericWebApplication
 {
@@ -22,6 +24,13 @@ namespace GenericWebApplication
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var keyVaultEndpoint = new System.Uri(Environment.GetEnvironmentVariable("VaultUri"));
+                    config.AddAzureKeyVault(
+                    keyVaultEndpoint,
+                    new DefaultAzureCredential());
+                })
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
