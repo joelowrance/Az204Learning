0728c8b5c05cbaa153afecd0857d15a581e94684
Joe <joelowrance@gmail.com>
2021-06-08 20:25:37 -0400

    Add cosmos db project and code to create the database.

---
+++ CosmosDb/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Serialization;
+
+namespace CosmosDb
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            InitializeConfiguration();
+            
+            var creator = new CreateDatabaseExample(Configuration);
+            //await creator.Main();
+                
+            Console.ReadKey();
+        }
+        
+        #region config
+
+        private static void InitializeConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+
+            Configuration = builder.Build();
+        }
+
+        public static IConfigurationRoot Configuration { get; set; }
+
+        #endregion
+    }
+
+    public class CreateDatabaseExample
+    {
+        private IConfigurationRoot _configurationRoot;
+
+        public CreateDatabaseExample(IConfigurationRoot configurationRoot)
+        {
+            _configurationRoot = configurationRoot;
+        }
+
+
+        public async Task Main()
+        {
+            var client = new CosmosClient(_configurationRoot["ConnectionString"]);
+            var container = await client.CreateDatabaseAsync(Names.DatabaseName);
+
+            var db = client.GetDatabase(Names.DatabaseName);
+            var response = await db.CreateContainerAsync(Names.ContainerName, Names.PartitionKey);
+            
+            
+            Console.WriteLine("Database and container have been created");
+            
+        }
+        
+    }
+
+    public static class Names
+    {
+        public static readonly string DatabaseName = "MyDb";
+        public static readonly string ContainerName = "Course";
+        public static readonly string PartitionKey = "/courseid";
+        
+    }
+}
