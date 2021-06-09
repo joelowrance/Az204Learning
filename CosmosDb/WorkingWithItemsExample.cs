26bad0c4ab7385bbd86f8d690b4cc1fdd92f291e
Joe <joelowrance@gmail.com>
2021-06-09 18:59:47 -0400

    Add examples of working with cosmosdb items.

--- CosmosDb/WorkingWithItemsExample.cs
+++ CosmosDb/WorkingWithItemsExample.cs
@@ -1,52 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Threading.Channels;
 using System.Threading.Tasks;
-using System.Threading.Tasks.Sources;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace CosmosDb
 {
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            InitializeConfiguration();
-         
-            // need to comment and uncomment as you go.
-            //var creator = new CreateDatabaseExample(Configuration);
-            //await creator.Main();
-
-            var workingWithItems = new WorkingWithItemsExample(Configuration);
-            //await workingWithItems.AddItemToDb();
-            //await workingWithItems.BulkAddItems();
-            //await workingWithItems.ReadItems();
-            //await workingWithItems.UpdateItems();
-            //await workingWithItems.DeleteItem();
-                
-            Console.ReadKey();
-        }
-        
-        #region config
-
-        private static void InitializeConfiguration()
-        {
-            var builder = new ConfigurationBuilder()
-                .AddUserSecrets<Program>();
-
-            Configuration = builder.Build();
-        }
-
-        public static IConfigurationRoot Configuration { get; set; }
-
-        #endregion
-    }
-
     public class WorkingWithItemsExample
     {
         private IConfigurationRoot _configurationRoot;
@@ -142,54 +101,5 @@ namespace CosmosDb
             
             Console.WriteLine(result.Resource.CourseName + " was deleted");
         }
-    }
-
-    public class CreateDatabaseExample
-    {
-        private IConfigurationRoot _configurationRoot;
-
-
-
-        public CreateDatabaseExample(IConfigurationRoot configurationRoot)
-        {
-            _configurationRoot = configurationRoot;
-        }
-
-
-        public async Task Main()
-        {
-            var client = new CosmosClient(_configurationRoot["ConnectionString"]);
-            var container = await client.CreateDatabaseAsync(Names.DatabaseName);
-
-            var db = client.GetDatabase(Names.DatabaseName);
-            var response = await db.CreateContainerAsync(Names.ContainerName, Names.PartitionKey);
-            
-            
-            Console.WriteLine("Database and container have been created");
-            
-        }
-        
-    }
-
-    public static class Names
-    {
-        public static readonly string DatabaseName = "MyDb";
-        public static readonly string ContainerName = "Course";
-        public static readonly string PartitionKey = "/courseid";
-        
-    }
-
-    public class Course
-    {
-        
-        //database id
-        [JsonProperty("id")]
-        public string Id { get; set; }
-        
-        // courseid is the partition key
-        [JsonProperty("courseid")]
-        public string CourseId { get; set; }
-        public string CourseName { get; set; }
-        public decimal Rating { get; set; }
     }
 }
